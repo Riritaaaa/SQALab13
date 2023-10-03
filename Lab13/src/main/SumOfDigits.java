db3e0fdb91cd2a0e3b177a9a123a2c5cc1d1ab13
BasTaks <taksin.cup@gmail.com>
2023-10-03 22:17:18 +0700

    fix error syntax

--- Lab13/src/main/SumOfDigits.java
+++ Lab13/src/main/SumOfDigits.java
@@ -1,12 +1,8 @@
 package main;
 
 public class SumOfDigits {
-<<<<<<< HEAD
-	public int SumOfDigits(int number) {
-=======
 	
 	public int sumOfDigits(int number){
->>>>>>> 69783f84be6d5c2db5280c21f72df557880a7b6a
 		int sum = 0;
 
 		while (number > 9) {
