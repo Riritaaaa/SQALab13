f5ec4abdc0fb9bf1ffd83839256dfcdbd75117ad
BasTaks <taksin.cup@gmail.com>
2023-10-03 22:35:34 +0700

    fix bug loop sum digit

--- Lab13/src/main/SumOfDigits.java
+++ Lab13/src/main/SumOfDigits.java
@@ -3,16 +3,17 @@ package main;
 public class SumOfDigits {
 	
 	public int sumOfDigits(int number){
-		int sum = 0;
-
-		while (number > 9) {
+		int sum;
+		while (number >= 10) {
+			sum = 0;
 			while (number != 0) {
 				int digit = number % 10; // Get the last digit
 				sum += digit; // Add the last digit to the sum
 				number /= 10; // Remove the last digit
 			}
+			number = sum;
+			return sum;
 		}
-		return sum;
 	}
 	
 }
