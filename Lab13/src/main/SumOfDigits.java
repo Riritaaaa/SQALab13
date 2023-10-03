d050b121d178625748700eec0f8e76f03bd20634
“Rungtawan <rungtawan.s@kkumail.com>
2023-10-03 23:36:01 +0700

    Refactor sumOfDigit method to 2 method (sumOfDigits, getOneDigit)

--- Lab13/src/main/SumOfDigits.java
+++ Lab13/src/main/SumOfDigits.java
@@ -2,18 +2,23 @@ package main;
 
 public class SumOfDigits {
 	
-	public int sumOfDigits(int number){
-		int sum;
-		while (number >= 10) {
-			sum = 0;
-			while (number != 0) {
-				int digit = number % 10; // Get the last digit
-				sum += digit; // Add the last digit to the sum
-				number /= 10; // Remove the last digit
-			}
-			number = sum;
-			return sum;
+	// Calculate each one-digit of number
+	public int sumOfDigits(int number) {
+        int sum = 0;
+        while (number != 0) {
+            int digit = number % 10; // Get the last digit
+            sum += digit; // Add the last digit to the sum
+            number /= 10; // Remove the last digit
+        }
+        return sum;
+    }
+	
+	// Check if number is one-digit and return the result
+	public int getOneDigit(int number){
+		while (number >= 10) {	
+			number = sumOfDigits(number);
 		}
+		return number;
 	}
 	
 }
